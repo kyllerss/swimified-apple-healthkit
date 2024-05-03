e134b3f9745f514b174484f873b1495d4c190fd1
Kyle Lomeli <klomeli@kyles-air.lan>
2024-05-03 19:17:46 -0400

    Updated to Capacitor 6.0.0

--- dist/plugin.js
+++ dist/plugin.js
@@ -5,8 +5,6 @@ var capacitorSwimifiedCapacitorHealthKit = (function (exports, core) {
 
 	exports.SwimifiedCapacitorHealthKit = SwimifiedCapacitorHealthKit;
 
-	Object.defineProperty(exports, '__esModule', { value: true });
-
 	return exports;
 
 })({}, capacitorExports);
